b91d87572c6c17df737e14851ed0cc4462e5c217
kader01agi <md.abdul.kader.ru@gmail.com>
2023-12-08 20:52:30 +0600

    upgraded

--- projectName/dj5doc/urls.py
+++ projectName/dj5doc/urls.py
@@ -3,6 +3,10 @@ from . import views
 
 urlpatterns = [
     path('', views.home),
-    path('start.html/', views.start),
-    path('tutorial02.html/', views.tutorial02),
+    path('start/', views.start),
+    path('tutorial02/', views.tutorial02),
+    path('codepractice/', views.codepractice),
+    path('marketplace/', views.marketplace),
+    path('about/', views.about),
+    path('contacts/', views.contacts),
 ]
--- projectName/dj5doc/views.py
+++ projectName/dj5doc/views.py
@@ -11,5 +11,21 @@ def start(request):
 def tutorial02(request):
     return render(request, 'dj5doc/tutorial02.html')
 
+def codepractice(request):
+    name = "MD. ABDUL KADER"
+    country = "Bangladesh"
+    skills = ["Python", "Django", "HTML"]
+    data = {"name": name, "country": country, "skills": skills}
+    return render(request, 'dj5doc/codepractice.html', data)
+
+def marketplace(request):
+    return render(request, 'dj5doc/marketplace.html')
+
+def about(request):
+    return render(request, 'dj5doc/about.html')
+
+def contacts(request):
+    return render(request, 'dj5doc/contacts.html')
+
 # def home(request):
 #     return HttpResponse('This is home test')
